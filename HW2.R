6f9177125c7f0c770bdbd8a101ffcbd8b0e7c147
Amber <zhangrui9399@gmail.com>
2016-02-18 02:25:14 -0500

    update HW2

--- HW2.R
+++ HW2.R
@@ -5,25 +5,38 @@ setwd("~/SIT/2016S/627/627DataAcquisition/")
 #Q2
 file.choose()
 Data2=read.csv("/Users/apple/SIT/2016S/627/week/week4/hw2/Q2.csv",header=TRUE)
+# plot the origin data with time
+a1<-ts(Data2) # creat time-series objects
+plot(a1,xlab="time",ylab="data2",main="Data2") 
+#acf and pacf
 lag<-100
 par(mfrow=c(2,1))
-acf(Data2[,2],lag.max=lag,plot=TRUE,main="Q2data ACF")
-pacf(Data2[,2],lag.max=lag,plot=TRUE,main="Q2data PACF")
+acf(Data2[,1],lag.max=lag,plot=TRUE,main="Q2data ACF")
+pacf(Data2[,1],lag.max=lag,plot=TRUE,main="Q2data PACF")
+
+
+#Q2 residuals
+t21<-arima(Data2,c(8,0,0)) % AR(8)
+t22<-arima(Data2,c(0,0,3)) % MA(3)
+par(mfrow=c(2,1))
+acf(t21$residuals,main="Data2 AR(8) Residuals")
+acf(t22$residuals,main="Data2 MA(3) Residuals")
+# get cofficients
+arima(Data2,order=c(0,0,3),method='ML')
+
+
 #Q3
 file.choose()
 Data3=read.csv("~/SIT/2016S/627/week/week4/hw2/Q3.csv",header = TRUE)
+a2<-ts(Data3) # creat time-series objects
+plot(a2,xlab="time",ylab="data3",main="Data3") 
+lag<-100
 par(mfrow=c(2,1))
 acf(Data3[,1],lag.max=lag,plot=TRUE,main="Q3data ACF")
 pacf(Data3[,1],lag.max=lag,plot=TRUE,main="Q3data PACF")
 
-#Q2 residuals
-tt<-arima(Data2,c(0,0,4))
-par(mfrow=c(2,1))
-acf(tt$residuals,main="Data2 ACF Residuals")
-pacf(tt$residuals,main="Data2 PACF Residuals")
-
 #Q3 residuals
-tt<-arima(Data3,c(4,1,0))
-par(mfrow=c(2,1))
-acf(tt$residuals,mian="Data3 ACF Residuals")
-pacf(tt$residuals,mian="Data3 PACF Residuals")
+t3<-arima(Data3,c(11,1,3)) #经过试验
+acf(t3$residuals,mian="Data3 ACF Residuals")
+# get cofficients
+arima(Data3,order=c(11,0,3),method='ML')
